83bd5459602e895a2ca5d94a6a829ece0c93f39a
Joshua Bassett <hello@joshbassett.info>
2019-07-03 21:01:40 +1000

    Refactor counter

--- src/counter.vhd
+++ src/counter.vhd
@@ -1,10 +1,9 @@
 library ieee;
-
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity counter is
-  port(
+  port (
     clk : in std_logic;
     led : out std_logic_vector(7 downto 0)
   );
